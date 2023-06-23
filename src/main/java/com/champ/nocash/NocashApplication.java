84dbf96d43cf806ac8799b1b3332e95a0082a95d
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-06-23 14:16:22 +0800

    Initial commit

---
+++ src/main/java/com/champ/nocash/NocashApplication.java
@@ -0,0 +1,13 @@
+package com.champ.nocash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocashApplication.class, args);
+	}
+
+}
