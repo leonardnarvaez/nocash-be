4db8866ab26dd82d7fca2bd9acb49c165f68a8f1
jaybee291998 <jaybee291998@gmail.com>
2023-07-09 23:49:58 +0800

    fixed the bug where we can logout if the token is not set, even thugh the endpoint is free for all

--- src/main/java/com/champ/nocash/security/JwtRequestFilter.java
+++ src/main/java/com/champ/nocash/security/JwtRequestFilter.java
@@ -32,7 +32,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
-            username = jwtUtil.extractUsername(jwt);
+            try {
+                username = jwtUtil.extractUsername(jwt);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
 
         if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
