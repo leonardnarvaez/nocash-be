567b7868da719ffc00e18d3f08512cf9753ad52e
jaybee291998 <jaybee291998@gmail.com>
2023-07-12 13:46:54 +0800

    fixed bug introduce by adding salt, and implemented service, and controller for transaction history

--- src/main/java/com/champ/nocash/repository/TransactionHistoryEntityRepository.java
+++ src/main/java/com/champ/nocash/repository/TransactionHistoryEntityRepository.java
@@ -4,6 +4,10 @@ import com.champ.nocash.collection.TransactionHistoryEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TransactionHistoryEntityRepository extends MongoRepository<TransactionHistoryEntity, String> {
+    List<TransactionHistoryEntity> findByDateBetweenAndUserId(LocalDateTime startDate, LocalDateTime endDate, String userId);
 }
--- src/main/java/com/champ/nocash/service/TransactionHistoryEntityService.java
+++ src/main/java/com/champ/nocash/service/TransactionHistoryEntityService.java
@@ -2,6 +2,11 @@ package com.champ.nocash.service;
 
 import com.champ.nocash.collection.TransactionHistoryEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransactionHistoryEntityService {
     TransactionHistoryEntity save(TransactionHistoryEntity transaction) throws Exception;
+
+    List<TransactionHistoryEntity> getAll(LocalDateTime startDate, LocalDateTime endDate);
 }
